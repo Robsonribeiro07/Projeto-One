7cbc96a7580dd74e64517bbc27fcdc5d80ce3e98
robsonribeiro07 <robsonrodriguez.007@gmail.com>
2024-11-09 19:25:50 -0300

    update

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ const button = switchh.querySelector('img');  // Seleciona a imagem
 const body = document.querySelector('body');
 const Name = document.querySelector('.profile-name')
 const container = document.querySelector('.container')
+const as = document.querySelectorAll('li')
+const iconElements = document.querySelectorAll('.social-medias ul li img'); // Seleciona todos os ícones
+const buttonVoltar = document.querySelector('.voltar')
+
+const switchhh = document.querySelector('.switch')
+const sectionLinks = document.querySelector('.section-links')
+
 
 const audio = document.querySelector('.selected')
 
@@ -12,19 +19,44 @@ const listas = document.querySelectorAll('span')
 const imgWhite = "url('/src/assets/svg/backgroundLight.svg')";  // Caminho da imagem de fundo
 const imgBlack = "url('/src/assets/svg/Background.svg')";  // Caminho da imagem de fundo
 
+const audio2 = document.createElement('audio')
+
+
+audio2.src = "/src/assets/audio/click-button-140881.mp3"
+document.body.appendChild(audio2)
+
+
 // Variável 'start' movida para fora para manter seu estado
 let start = false;
 
+const IconsBlack = [
+    "/src/assets/svg/githubBlack.svg",
+    "/src/assets/svg/youtubeBlack.svg",
+    "/src/assets/svg/instagramBlack.svg",
+    "/src/assets/svg/linkedinBlack.svg",
+]
+const icnonsWhite = [
+    "/src/assets/svg/github.svg",
+    "/src/assets/svg/youtube.svg",
+    "/src/assets/svg/instagram.svg",
+    "/src/assets/svg/linkdin.svg",
+
+
+]
 // Aplica o evento de clique apenas na imagem e impede a propagação imediata
 button.addEventListener('click', (e) => {
+ 
     e.stopImmediatePropagation(); // Impede a propagação do evento para outros ouvintes
 
     if (!start) {
         button.style.transform = "translateX(40px)";  // Move a imagem para a direita
         AlterarThemaWhite();  // Alterando o tema ao clicar
+        audio2.play()
     } else {
         button.style.transform = "translateX(0)";  // Retorna a imagem à posição inicial
         AlterarThemaBlack()
+        audio2.play()
+
     }
 
     // Alterna o estado de 'start'
@@ -36,19 +68,44 @@ console.log('ok');
 // Função para alterar o tema de fundo
 function AlterarThemaWhite() {
     setTimeout(() => {
+        const containerMensagems = document.querySelector('.section-mensagens')
+       containerMensagems.style.backgroundColor = "orange"
         body.style.backgroundImage = imgWhite;
         Name.style.color = "black"
+         
+        listas.forEach(items =>{
+           items.style.color = "black"
+        })
+
+         
+        // Para cada ícone, alteramos o src
+        iconElements.forEach((icon, index) => {
+            icon.src = IconsBlack[index]; // Atribuímos o novo ícone baseado no array
+        });
+    
 
     }, 100);  // Atraso de 100ms para garantir que a animação do botão ocorra primeiro
 }
 
 function AlterarThemaBlack() {
-    setTimeout(() => {
-        body.style.backgroundImage = imgBlack;
-        Name.style.color = "White"
-    }, 100);  // Atraso de 100ms para garantir que a animação do botão ocorra primeiro
-}
 
+    setTimeout(() => {
+        const containerMensagems = document.querySelector('.section-mensagens')
+       containerMensagems.style.backgroundColor = "white"
+
+        body.style.backgroundImage = imgBlack;
+        Name.style.color = "White" 
+        listas.forEach(items =>{
+            items.style.color = "White"
+         })
+
+
+         iconElements.forEach((icon, index)=>[
+            icon.src = icnonsWhite[index]
+         ])
+    }, 100);  // Atraso de 100ms para garantir que a animação do botão ocorra primeiro
+
+}
 let currentIndex = 0
 function selectItem(index) {
     // Remover a classe de todos os itens
@@ -95,4 +152,27 @@ document.addEventListener('keydown', (e)=>{
     } else if(e.key ==="ArrowUp"){
         moveUp()
     }
+})
+listas.forEach(list =>{
+    list.addEventListener('click', ()=>{
+        audio2.play()
+    })
+})
+listas.forEach((item )=>{
+   if(item.hasAttribute('data-section')){
+
+    item.addEventListener('click', ()=>{
+        const containerMensagems = document.querySelector('.container-mensagems')
+        sectionLinks.classList.add('disabled')
+        containerMensagems.classList.remove('disabled')
+
+    })
+
+   }
+})
+buttonVoltar.addEventListener('click',()=>{
+    const containerMensagems = document.querySelector('.container-mensagems')
+    sectionLinks.classList.remove('disabled')
+    containerMensagems.classList.add('disabled')
+    audio2.play()
 })
---
+++ scriptMensagem.js
@@ -0,0 +1,138 @@
+const containerMensagems = document.querySelector(".show-mensagem");
+const SendButton = document.querySelector('.sendButton');
+const meensagemuser1 = document.querySelector('.mensagemUser1');
+const meensagemuser2 = document.querySelector('.mensagemUser2');
+const mensagemInfo = document.querySelector('b')
+
+const anexaImage = document.querySelector('.anexaImage')
+const imageInput = document.querySelector(".imageInput")
+
+const inputText = document.querySelector('input');
+const animationDigitaçao = document.querySelectorAll('.dot')
+
+const newAudio = new Audio('/src/assets/audio/digitaçao.mp3')
+const deleteAudio = new Audio('/src/assets/audio/delete.mp3')
+let mensagensUser1 = []; // Array para armazenar as mensagens do usuário 1
+let mensagensUser2 = []; // Array para armazenar as mensagens do usuário 2
+
+let mensagemUser1 = false; // Alterna entre os usuários
+
+
+let imageMessage = ''
+
+
+SendButton.addEventListener('click', () => {
+    if (inputText.value.trim() === '') return;
+
+    const textInput = inputText.value;
+    const newMensagem = document.createElement('div');
+    const img = document.createElement('img');
+    img.src = "/src/assets/svg/profile.svg";
+    const newMensagems = document.createElement('p');
+    newMensagems.innerHTML = textInput;
+
+    const deleteButton = document.createElement('img');
+    deleteButton.src = "/src/assets/img/image.png"
+
+    // Adiciona evento para excluir a mensagem
+    deleteButton.addEventListener('click', () => {
+        apagarMensagem(newMensagem, mensagemUser1);
+    });
+
+    if (!mensagemUser1) {
+        newMensagem.classList.add("mensagem");
+        newMensagem.appendChild(img);
+        newMensagem.appendChild(newMensagems);
+        newMensagem.appendChild(deleteButton);
+        meensagemuser1.appendChild(newMensagem);
+    deleteButton.classList.add('deleteButton');
+        
+        // Adiciona a mensagem ao array de mensagens do usuário 1
+        mensagensUser1.push({ id: Date.now(), texto: textInput });
+    } else {
+        newMensagem.classList.add("mensagem2");
+        newMensagem.appendChild(newMensagems);
+        newMensagem.appendChild(img);
+        newMensagem.appendChild(deleteButton);
+        meensagemuser2.appendChild(newMensagem);
+    deleteButton.classList.add('deleteButton2');
+        
+        // Adiciona a mensagem ao array de mensagens do usuário 2
+        mensagensUser2.push({ id: Date.now(), texto: textInput });
+    }
+    EnvioAtualizaçao()
+    mensagemUser1 = !mensagemUser1;
+    inputText.value = '';
+});
+
+// Função para apagar mensagem específica
+function apagarMensagem(element, isUser1) {
+    if (isUser1) {
+        mensagensUser1 = mensagensUser1.filter(msg => msg.texto !== element.querySelector('p').innerHTML);
+        deleteAudio.currentTime = 0
+        deleteAudio.play()
+    } else {
+        mensagensUser2 = mensagensUser2.filter(msg => msg.texto !== element.querySelector('p').innerHTML);
+        deleteAudio.play()
+        deleteAudio.currentTime = 0
+
+
+    }
+    element.remove();
+}
+inputText.addEventListener('input', ()=>{
+    if(inputText.value.trim() !== ''){
+        newAudio.play()
+        newAudio.currentTime = 0
+
+    }
+
+    animationDigitaçao.forEach(animation =>{
+        animation.classList.remove('disabled')
+    })
+})
+inputText.addEventListener('blur', () => {
+    newAudio.pause();
+    newAudio.currentTime = 0;    
+    animationDigitaçao.forEach(animation =>{
+        animation.classList.add('disabled')
+    })      // Reinicia o áudio para a próxima digitação
+});
+
+function EnvioAtualizaçao(){
+    mensagemInfo.innerText = "Mensagem enviada com sucesso"
+
+     setTimeout(() => {
+        mensagemInfo.innerText = ""
+     }, 400);
+}
+anexaImage.addEventListener('click', ()=>{
+    imageInput.click()
+})
+imageInput.addEventListener('change', () => {
+    const file = imageInput.files[0];
+    
+    if (file) {
+        const reader = new FileReader();
+
+        reader.onload = function(event) {
+            // Armazena a imagem em Base64 na variável `imageMessage`
+            imageMessage = event.target.result;
+            
+            // Exibe a imagem na pré-visualização
+            previewImage.src = imageMessage;
+            previewImage.style.display = 'block';
+        };
+
+        // Converte a imagem para Base64
+        reader.readAsDataURL(file);
+    } 
+    const newMensagem = document.createElement('div')
+    newMensagem.classList.add('mensagem')
+    const newImage = document.createElement('img')
+    newImage.src = imageMessage
+
+    newMensagem.appendChild(newImage)
+
+
+});
